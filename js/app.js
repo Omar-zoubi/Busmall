e0cd24ea2e954cada06aa2cc5c4345a6ea2dc94e
Omar-zoubi <o.fandi02@gmail.com>
2021-02-22 09:07:38 +0200

    chartdrawing

--- js/app.js
+++ js/app.js
@@ -12,6 +12,13 @@ let cont =document.getElementById('div1');
 let ulist= document.getElementById('unlist');
 let objArr =[];
 
+let perArr=[0,0,0];
+let currVal=[0,0,0];
+
+let voteArr=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+let showenArr=[];
+let objectNmae=[];
+
 
 
 function Market(name ,source){
@@ -50,6 +57,8 @@ let firstIn =0;
 let secondIn=0;
 let theirdIn=0;
 
+
+
 function random_poss()
 {
     let randomIndex = Math.floor(Math.random() * objArr.length); 
@@ -58,7 +67,7 @@ function random_poss()
 }
 
 
-function renderThree()
+function generatThree()
 {
     firstIn=random_poss();
     secondIn=random_poss();
@@ -70,35 +79,62 @@ function renderThree()
         secondIn=random_poss();
         theirdIn=random_poss();
     }
-    objArr[firstIn].show++;
-    objArr[secondIn].show++;
-    objArr[theirdIn].show++;
+  
+    currVal=[firstIn,secondIn,theirdIn];
+
+}
 
 
+function renderThree()
+{
     img1.setAttribute('src',objArr[firstIn].source);
     img2.setAttribute('src',objArr[secondIn].source);
     img3.setAttribute('src',objArr[theirdIn].source);
-
-
+  
 
 }
+
+function noRepeat()
+{
+   
+for (let i=0;i<currVal.length;i++)
+{
+   while (perArr.includes(currVal[i]))
+{
+    generatThree();
+}
+
+}
+objArr[firstIn].show++;
+objArr[secondIn].show++;
+objArr[theirdIn].show++;
+
+ perArr=currVal;
+}
+generatThree();
+noRepeat();
 renderThree();
+
 
 function choosImg(event)
 {    
-
     attempt++;
     if (attempt<=maxClick)
     {
         if (event.target.id==="firstimg")
         {
             objArr[firstIn].vote++;
+            voteArr[firstIn]=objArr[firstIn].vote;
         }else if (event.target.id==="secondimg")
         {
             objArr[secondIn].vote++;
+            voteArr[secondIn]=objArr[secondIn].vote;
         }else {
             objArr[theirdIn].vote++;
+            voteArr[theirdIn]=objArr[theirdIn].vote;
         }
+        generatThree();
+        noRepeat();
         renderThree();
 
     }else
@@ -106,6 +142,7 @@ function choosImg(event)
         cont.removeEventListener('click', choosImg);
 
         button.addEventListener('click',resultFun);
+        
     }
 }
 
@@ -121,7 +158,48 @@ function resultFun()
      unorList.appendChild(li);
      li.textContent=`${i+1}. ${objArr[i].name} has ${objArr[i].vote} vote and it had been showen ${objArr[i].show} times.`;
 
-    }
+}
+console.log(voteArr);
+    chartrender();
     button.removeEventListener('click',resultFun);
 
+}
+
+
+function chartrender(){
+    for (let i=0;i<objArr.length;i++)
+{
+    objectNmae.push(objArr[i].name);
+    showenArr.push(objArr[i].show);
+}
+var chrtJs= document.getElementById('score').getContext('2d');
+console.log(showenArr);
+console.log(voteArr);
+
+var buyerData = new Chart(chrtJs,  {
+    type: 'bar',
+
+	
+    data:{
+        labels : objectNmae,
+	datasets : [
+		{
+            label : 'Product Voiting System',
+            backgroundColor: 'rgba(12,194,12,0.4)',
+            borderColor :'ACC26D',
+			data : voteArr,
+		}
+        ,
+        {
+            label : 'Time of diplay',
+            backgroundColor: 'gray',
+            borderColor :'black',
+			data : showenArr,
+		}
+        
+
+    ]
+}
+
+})
 }
